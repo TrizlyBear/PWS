a27b94cee60d97bbf1a644a92b9153433982e15b
Tristan <tristanschmidt2004@gmail.com>
2021-05-04 18:38:48 +0200

    Fixed workflow for saving test

--- testing/saving_test.go
+++ testing/saving_test.go
@@ -5,7 +5,6 @@ import (
 	"github.com/TrizlyBear/PWS/sequential"
 	"github.com/TrizlyBear/PWS/sequential/activation"
 	"github.com/TrizlyBear/PWS/sequential/layers"
-	"os"
 	"testing"
 )
 
@@ -15,8 +14,7 @@ func TestSave(t *testing.T) {
 	XORy := [][][]float64{{{0.0}}, {{1.0}}, {{1.0}}, {{0.0}}}
 	n := &sequential.Model{Layers: []sequential.Layer{&layers.FC{Out: 10},&activation.Tanh{}, &layers.FC{Out: 1},&activation.Tanh{}}}
 	n.Fit(XORx, XORy, 1000, 0.1)
-	os.Mkdir("../models",os.ModeAppend)
-	err := n.Save("../models","test")
+	err := n.Save("./models","test")
 	if err != nil {
 		fmt.Println(err)
 	}
