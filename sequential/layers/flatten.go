287c617258f746bc05a32578875f413eb0a71f1c
Tris <38221328+TrizlyBear@users.noreply.github.com>
2021-07-08 13:58:58 +0200

    Update flatten.go

--- sequential/layers/flatten.go
+++ sequential/layers/flatten.go
@@ -4,14 +4,18 @@ import (
 	"github.com/TrizlyBear/PWS/math"
 )
 
+// Flatten layer
 type Flatten struct {
 	y int
 	x int
 }
 
+// Flattern layer forward function
 func (e *Flatten) Forward(in [][]float64) [][]float64 {
+	// Saves the dimensions for the error computation
 	e.y = len(in)
 	e.x = len(in[0])
+	
 	out := []float64{}
 	for _, x := range in {
 		for _, y := range x {
@@ -22,6 +26,8 @@ func (e *Flatten) Forward(in [][]float64) [][]float64 {
 	return [][]float64{out}
 }
 
+// Flatten backward function
 func (e *Flatten) Backward(err [][]float64, float642 float64) [][]float64 {
+	// Resize the output to the input dimensions
 	return math.Resize(err, e.y, e.x)
 }
