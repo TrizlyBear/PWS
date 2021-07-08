bb7fec6dffe2bfb5776e124e77ca4ade5e81bfa6
Tris <38221328+TrizlyBear@users.noreply.github.com>
2021-07-08 13:41:01 +0200

    Update functions.go

--- sequential/activation/functions.go
+++ sequential/activation/functions.go
@@ -2,20 +2,24 @@ package activation
 
 import "math"
 
+// Rectifier linear unit layer
 type ReLu struct {
 	input [][]float64
 }
 
+// ReLu forward
 func (e *ReLu) Forward(in [][]float64) [][]float64 {
 	(*e).input = in
 	for y,el := range in {
 		for x,_ := range el {
+			
 			in[y][x] = math.Max(0, in[y][x])
 		}
 	}
 	return in
 }
 
+// ReLu backward function
 func (e *ReLu) Backward(err [][]float64, lr float64) [][]float64 {
 	for y,el := range err {
 		for x,_ := range el {
@@ -28,23 +32,28 @@ func (e *ReLu) Backward(err [][]float64, lr float64) [][]float64 {
 	return err
 }
 
+// Hyperbolic tangent layer
 type Tanh struct {
 	Input [][]float64
 }
 
+// Tanh Forward function
 func (e *Tanh) Forward(in [][]float64) [][]float64 {
 	(*e).Input = in
 	for y,el := range in {
 		for x,_ := range el {
+			// Set every value to tanh(value)
 			in[y][x] = math.Tanh(in[y][x])
 		}
 	}
 	return in
 }
 
+// Tanh backward function
 func (e Tanh) Backward(err [][]float64, lr float64) [][]float64 {
 	for y,el := range err {
 		for x,_ := range el {
+			// Multiply the error with the tanh derivative of itself
 			err[y][x] *= (1 - math.Pow(math.Tanh(e.Input[y][x]),2))
 		}
 	}
